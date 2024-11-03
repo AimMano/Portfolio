1f3b431336a01d899eab6f14fd43f077d0be7ff6
Manoranjan kumar <153417773+AimMano@users.noreply.github.com>
2024-11-03 21:22:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+gsap.from(".navlist li" ,{
+    y:-80,
+    position:3,
+    delay:1,
+ stagger:0.15,
+ opacity:0
+})
+gsap.from(".Intro",{
+    x:-100,
+    postion:3,
+delay:2,
+opacity:0,
+})
+gsap.from(".content h1",{
+    x:100,
+    postion:3,
+delay:3,
+opacity:0,
+})
+gsap.from(".details",{
+    y:100,
+    postion:2,
+delay:4,
+opacity:0,
+})
+gsap.from(".icons i",{
+    x:-100,
+    postion:3,
+delay:1,
+opacity:0,
+stagger: 0.15
+})
+gsap.from(".circle",{
+    x:150,
+    postion:3,
+delay:2,
+opacity:0,
+})
+gsap.from(".btn_container",{
+    y:80,
+    postion:3,
+delay:5,
+opacity:0
+})
